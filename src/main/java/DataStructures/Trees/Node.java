d560c6375fe94074eec6b70d06e44609d60e4169
1570599338@qq.com <1570599338@qq.com>
2018-07-15 04:01:21 +0800

    其他的

---
+++ src/main/java/DataStructures/Trees/Node.java
@@ -0,0 +1,11 @@
+package DataStructures.Trees;
+
+class Node{
+    int data;
+    Node left, right;
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,34 @@
+/**
+ * 对于多重循环情况下，遇到return相当于推出for循环了 
+ * @author lquan
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(prime(40));
+
+	}
+	
+	   public static boolean prime(int num) { // checking if number is prime or not
+		   int count1=0;
+		   int count2=0;
+		   for (int j = 2; j <= num / 2; j++) {
+			   count1++;
+			   System.out.println(count1);
+	        for (int divisor = 2; divisor <= num / 2; divisor++) {
+	        	count2++;
+	        	System.out.println(count2);
+	            if (num % divisor == 0) {
+	                return false; //  false if not prime
+	            }
+	        }
+	        
+	        return true; // True if prime
+	    }
+		   return false;
+	   }
+	   
+
+}
