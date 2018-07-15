adc5968dfb609f62928f4b620492921340b63cae
1570599338@qq.com <1570599338@qq.com>
2018-07-16 01:25:08 +0800

    队列，数组，栈

---
+++ src/main/java/com/lquan/charp4/Tree/four/one/one/TreeNode.java
@@ -0,0 +1,48 @@
+
+package com.lquan.charp4.Tree.four.one.one;   
+
+/**   
+
+ * Filename:    TreeNode.java   
+
+ * Copyright:   Copyright (c)2010  
+
+ * @author  :   lquan
+
+ * E-mail   :	lquan.liu@outlook.com
+
+ * @version :   1.0   
+
+ * @since   :  	JDK 7
+
+ * Create at:   Jul 15, 2018 1:43:35 PM   
+
+ * Description:  4.1.1 树的实现 具体请参考p103
+
+ *   
+
+ * Modification History:   
+
+ * Date    Author      Version     Description   
+
+ * ----------------------------------------------------------------- 
+
+ * Jul 15, 2018 lquan      1.0     1.0 Version   
+
+ */
+
+public class  TreeNode<T> {
+	T element;
+	TreeNode<T> fristChild;
+	TreeNode<T> nextChild;
+
+}
+  
+//public class  TreeNode {
+//	Object element;
+//	TreeNode fristChild;
+//	TreeNode nextChild;
+//
+//}
+
+
