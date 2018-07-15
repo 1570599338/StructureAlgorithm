8ad4e82335b8aad4e0d11e0ea5dd683084b00af3
1570599338@qq.com <1570599338@qq.com>
2018-07-15 12:57:57 +0800

    重新修改包路径

---
+++ src/main/java/algorithm/compression/compressclient.java
@@ -0,0 +1,12 @@
+package algorithm.compression;
+
+public class compressclient {
+
+	public static void main(String[] args) {
+
+		HEncoder h= new HEncoder("aaaabbbcccccccccccdddd");
+		System.out.println(h.compress("aabccd"));
+		System.out.println(h.decompress("101011000111"));
+	}
+
+}
